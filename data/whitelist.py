681959e46b960b895c24eaadb08bdb7285b23fc3
Revathi Brungi <revathideveloperbrungi@gmail.com>
2026-03-25 13:36:18 +0530

    Create whitelist.py

---
+++ data/whitelist.py
@@ -0,0 +1,95 @@
+THREEPC = "3rd Party Controller"
+ABM = "ABM"
+ACM = "ACM"
+ACLM = "ACLM"
+ADE = "Ackumen Data Entry"
+ALARM_AUTOMATION = "Alarm Automation"
+ALARM_AUTOMATION_ESCALATION = "Alarm Automation Escalation"
+ALARMS = "Alarms"
+CALCULATED_PARAMETERS = "Calculated Parameters"
+CALIBRATION = "Calibration"
+CHARTS = "Charts"
+CHEMICAL_ORDERING = "Chemical Ordering"
+CLOAK = "Cloak"
+CONNECTED_ASSETS = "Connected Assets"
+CONNECTED_LAB = "Connected Lab"
+CONNECTED_PLANNING = "Connected Planning"
+DPS = "DPS"
+DYNAMIC_IO = "Dynamic I/O"
+EQUIPMENT_ORDERING = "Equipment Ordering"
+HSI = "Halogen Sensor Integration"
+HEADSPACE = "Headspace"
+KPM = "KPM"
+MCAI = "MCA-i"
+MODULAR_CONTROLLER = "Modular Controller"
+NOTIFICATION_AND_EVENTS = "Notifications and Events"
+PRICE_MANAGEMENT = "Price Management"
+PRICING_DOT = "Pricing Dot"
+PRICING_INFRA = "Pricing Infrastructure"
+PROCESSVIEW = "ProcessView"
+REVENUE_FORECASTING = "Revenue Forecasting"
+SCC = "SCC"
+SCC_SUMMARY = "SCC Summary"
+SMART_DISPENSER = "Smart Dispenser"
+SMART_INVENTORY = "Smart Inventory"
+SMART_PAIL = "Smart Pail"
+SMART_PUMP = "Smart Pump"
+TITAN = "Titan"
+
+whitelisted_features = [
+    THREEPC,
+    ABM,
+    ACM,
+    ACLM,
+    ADE,
+    ALARM_AUTOMATION,
+    ALARM_AUTOMATION_ESCALATION,
+    ALARMS,
+    CALCULATED_PARAMETERS,
+    CALIBRATION,
+    CHARTS,
+    CHEMICAL_ORDERING,
+    CLOAK,
+    CONNECTED_ASSETS,
+    CONNECTED_LAB,
+    CONNECTED_PLANNING,
+    DPS,
+    DYNAMIC_IO,
+    EQUIPMENT_ORDERING,
+    HSI,
+    HEADSPACE,
+    KPM,
+    MCAI,
+    MODULAR_CONTROLLER,
+    NOTIFICATION_AND_EVENTS,
+    PRICE_MANAGEMENT,
+    PRICING_DOT,
+    PRICING_INFRA,
+    PROCESSVIEW,
+    REVENUE_FORECASTING,
+    SCC,
+    SCC_SUMMARY,
+    SMART_DISPENSER,
+    SMART_INVENTORY,
+    SMART_PAIL,
+    SMART_PUMP,
+    TITAN,
+]
+
+whitelisted_priority = [
+    "1",
+    "2",
+    "3",
+    "4",
+]
+
+whitelisted_severity = [
+    "1 - Critical",
+    "2 - High",
+    "3 - Medium",
+    "4 - Low",
+]
+
+whiteliested_state = ["Done", "Ready for Prod"]
+
+whiteliested_file_types = [".cs", ".js", ".html", ".sql", ".py"]
